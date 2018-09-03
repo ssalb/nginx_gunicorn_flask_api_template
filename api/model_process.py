7df09b1777951b4410d4181db46cb6fb630aa1a3
Salvador Salazar <40628560+salalb@users.noreply.github.com>
2018-09-03 15:40:45 +0200

    formatting spaces

--- api/model_process.py
+++ api/model_process.py
@@ -11,37 +11,36 @@ if __name__=="__main__":
 
     while True:
         queue = db.lrange(config.DB_QUEUE, 0, config.BATCH_SIZE - 1)
-		q_ids = []
-		batch = None
-
+	q_ids = []
+	batch = None
+	
         for q in queue:
-			q = json.loads(q.decode("utf-8"))
-			input_x = q["input"]
+	    q = json.loads(q.decode("utf-8"))
+	    input_x = q["input"]
             ####################################################
             #
             # If required, preprocess input_x here
             # e.g. from list to np.array
             #
             ####################################################
-			if batch is None:
-				batch = input_x
-			else:
-				batch = np.vstack([batch, input_x])
-
-			q_ids.append(q["id"])
-
+	    if batch is None:
+	        batch = input_x
+	    else:
+                batch = np.vstack([batch, input_x])
+	    
+	    q_ids.append(q["id"])
+	
         if len(q_ids) > 0:
-			preds = model.predict(batch)
-
-			for (q_id, result) in zip(q_ids, results):
-				output = {"prediction": result}
+	    preds = model.predict(batch)
+	    for (q_id, result) in zip(q_ids, results):
+	        output = {"prediction": result}
                 #########################################
                 #
                 # Add other metadata to output here.
                 #
                 #########################################
-				db.set(q_id, json.dumps(output))
-
-			db.ltrim(settings.DB_QUEUE, len(q_ids), -1)
-
+	        db.set(q_id, json.dumps(output))
+	    	
+	    db.ltrim(settings.DB_QUEUE, len(q_ids), -1)
+	
         time.sleep(config.SERVER_SLEEP)
