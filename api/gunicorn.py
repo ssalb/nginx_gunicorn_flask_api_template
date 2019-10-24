5460b07565838186ed433febe52c5c51e58a5c2d
Salvador Salazar <40628560+ssalb@users.noreply.github.com>
2019-10-24 12:08:47 +0200

    Update gunicorn.py

--- api/gunicorn.py
+++ api/gunicorn.py
@@ -6,11 +6,11 @@ def max_workers():
     return cpu_count()
 
 
-bind = "0.0.0.0:{}".format(environ.get("API_PORT", 5000))
+bind = "127.0.0.1:{}".format(environ.get("API_PORT", 5000))
 max_requests = 1024
 workers = max_workers()
 
-accesslog = "./logs/gunicorn-access.log"
-errorlog = "./logs/gunicorn-error.log"
+# accesslog = "./logs/gunicorn-access.log"
+# errorlog = "./logs/gunicorn-error.log"
 loglevel = "info"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Real-IP}i)s"'
